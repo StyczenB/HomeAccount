717e5c1c37789f130981e442ca73282a6d9d4e9a
Bartłomiej <styczenb@student.agh.edu.pl>
2019-02-19 20:15:56 +0100

    Add the simpleset of all main class

---
+++ main.java
@@ -0,0 +1,17 @@
+class HomeAccount
+{
+    public static void main(String args[]) 
+    {
+        Apartment x = new Apartment();
+
+        // System.out.println("DUPSKO");    
+        // x.printUsers();
+        // x.addUser("Kuba");
+        // x.printUsers();
+        // x.addMoneyToUser(0, 1.34);
+        // x.addUser("Marcin");
+        // x.addMoneyToUser(1, 10.34);
+        // x.printUsers();
+        
+    }
+}
